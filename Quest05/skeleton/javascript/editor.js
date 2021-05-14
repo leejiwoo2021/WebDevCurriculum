a39bf0454fde91c0c73740e3a42f3a8010b5adeb
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-05-14 16:11:35 +0900

    feat : Quest05 contentEditable 속성 적용

--- Quest05/skeleton/javascript/editor.js
+++ Quest05/skeleton/javascript/editor.js
@@ -4,44 +4,11 @@ class Editor {
     this.#init();
   }
   #init() {
-    this.#addEvent();
     this.#initNewLine();
-  }
-
-  #addEvent() {
-    this.#addkeydownEvent();
   }
 
   #initNewLine() {
     const newLine = new Line().getElement();
     this.#editorElement.appendChild(newLine);
-  }
-
-  #addkeydownEvent() {
-    document.addEventListener('keydown', function (e) {
-      const activeElement = document.activeElement;
-      const key = e.key;
-
-      if (activeElement.className === 'l-editor-line t-editor-line') {
-        switch (key) {
-          case 'Enter': {
-            const newLine = new Line().getElement();
-            activeElement.after(newLine);
-            newLine.focus();
-            break;
-          }
-          case 'ArrowUp': {
-            const previousLine = activeElement.previousElementSibling;
-            if (previousLine) previousLine.focus();
-            break;
-          }
-          case 'ArrowDown': {
-            const nextLine = activeElement.nextElementSibling;
-            if (nextLine) nextLine.focus();
-            break;
-          }
-        }
-      }
-    });
   }
 }
--- Quest05/skeleton/javascript/line.js
+++ Quest05/skeleton/javascript/line.js
@@ -7,65 +7,12 @@ class Line {
 
   #init() {
     this.#createLineElement();
-    this.#addEvent();
   }
 
   #createLineElement() {
     const lineTemplate = document.querySelector('#template-editor-line');
     const lineClone = document.importNode(lineTemplate.content, true);
     this.#lineElement = lineClone.querySelector('p');
-  }
-
-  #addEvent() {
-    this.#addClickEvent();
-    this.#addKeydownEvent();
-  }
-
-  #addClickEvent() {
-    const lineElement = this.#lineElement;
-    lineElement.addEventListener('click', function (e) {
-      lineElement.focus();
-    });
-  }
-
-  #addKeydownEvent() {
-    const lineElement = this.#lineElement;
-    let meta = false;
-
-    lineElement.addEventListener('keydown', function (e) {
-      const key = e.key;
-
-      switch (key) {
-        case 'Backspace': {
-          const currentText = lineElement.innerHTML;
-          lineElement.innerHTML = currentText.substr(0, currentText.length - 1);
-          break;
-        }
-        case 'Meta': {
-          meta = true;
-          break;
-        }
-        case 'Enter': {
-          break;
-        }
-        default: {
-          if (!meta) {
-            lineElement.innerHTML += key;
-          }
-        }
-      }
-
-      lineElement.addEventListener('keyup', keyUpHandler);
-
-      function keyUpHandler(e) {
-        const key = e.key;
-        switch (key) {
-          case 'Meta':
-            meta = false;
-        }
-        lineElement.removeEventListener('keyup', keyUpHandler);
-      }
-    });
   }
 
   getElement() {
