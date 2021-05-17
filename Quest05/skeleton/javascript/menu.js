bac480fd62b7d6720c7f453fe7ed23a550113b85
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-05-17 13:02:34 +0900

    feat : Quest05 File 저장기능 구현

--- Quest05/skeleton/javascript/menu.js
+++ Quest05/skeleton/javascript/menu.js
@@ -2,6 +2,19 @@ class Menu {
   #saveButton = document
     .querySelector('.l-menu-container.t-menu-container')
     .querySelector('button');
+  #storage = new Storage();
+  constructor() {
+    this.#addSaveEvent();
+  }
 
-  constructor() {}
+  #addSaveEvent() {
+    const storage = this.#storage;
+    this.#saveButton.addEventListener('click', function () {
+      const fileName = Explorer.getActiveFileName();
+      if (fileName) {
+        const contents = Editor.getContent();
+        storage.saveFile(fileName, contents);
+      }
+    });
+  }
 }
