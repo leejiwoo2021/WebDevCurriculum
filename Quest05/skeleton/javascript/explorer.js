1abb3c22efab8236685487583c141ef17870c23e
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-05-17 12:50:05 +0900

    feat : Quest05 file explorer 활성화 theme 적용

--- Quest05/skeleton/javascript/explorer.js
+++ Quest05/skeleton/javascript/explorer.js
@@ -1,6 +1,7 @@
 class Explorer {
   #navElement = document.querySelector('.l-nav-container.t-nav-contaier');
   #editor = new Editor();
+
   constructor() {}
 
   setFileList(fileNameList) {
@@ -24,10 +25,26 @@ class Explorer {
 
     const editor = this.#editor;
 
-    buttonElement.addEventListener('click', function (e) {
+    buttonElement.addEventListener('click', function () {
+      Explorer.setButtonActive(name);
       editor.showFile(name);
     });
 
     return buttonElement;
+  }
+
+  static setButtonActive(name) {
+    [
+      ...document.querySelector('.l-nav-container.t-nav-contaier').children,
+    ].forEach((button) => {
+      const fileName = button.querySelector('h2').innerHTML;
+      if (name === fileName) {
+        button.classList.remove('t-nav-button');
+        button.classList.add('t-nav-button-active');
+      } else {
+        button.classList.remove('t-nav-button-active');
+        button.classList.add('t-nav-button');
+      }
+    });
   }
 }
