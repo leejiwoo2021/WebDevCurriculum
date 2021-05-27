82da890a3060bc7eb57a46b5aa26b48e268e5514
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-05-27 16:31:10 +0900

    feat : Quest07 Quest 구현

--- Quest07/skeleton/cjs-my-project/index.js
+++ Quest07/skeleton/cjs-my-project/index.js
@@ -1,1 +1,22 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+
+const cjsPkg = require('../cjs-package/index.js');
+
+const cjsClass = new cjsPkg.CjsUtilClass(5);
+
+console.log(cjsClass.foo);
+cjsClass.double();
+console.log(cjsClass.foo);
+console.log(cjsPkg.cjsUtilFunction('aAbBcC'));
+
+(async function esmImport() {
+  const { EsmUtilClass, esmUtilFunction } = await import(
+    '../esm-package/index.mjs'
+  );
+
+  const esmClass = new EsmUtilClass(3);
+  console.log(esmClass.foo);
+  esmClass.double();
+  console.log(esmClass.foo);
+  console.log(esmUtilFunction('dDeEfF'));
+})();
--- Quest07/skeleton/esm-package/index.mjs
+++ Quest07/skeleton/esm-package/index.mjs
@@ -1,15 +1,16 @@
 class EsmUtilClass {
-    constructor(foo) {
-        this.foo = foo;
-    }
+  constructor(foo) {
+    this.foo = foo;
+  }
 
-    double() {
-        return foo * 2;
-    }
+  double() {
+    return this.foo * 2;
+  }
 }
 
-const esmUtilFunction = str => {
-    return str.toUpperCase();
+const esmUtilFunction = (str) => {
+  return str.toUpperCase();
 };
 
+export { EsmUtilClass, esmUtilFunction };
 // TODO: 다른 패키지가 EsmUtilClass와 esmUtilFunction를 가져다 쓰려면 어떻게 해야 할까요?
