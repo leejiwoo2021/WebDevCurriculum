6f2ef335a3e350c1f87b3b3b54bd969ac551c1f8
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-05-31 09:16:20 +0900

    refactor : package.json 키워드 추가 - script, type, main 필드 추가 - npm install 및 npm run start cli 적용

--- Quest07/skeleton/cjs-my-project/index.js
+++ Quest07/skeleton/cjs-my-project/index.js
@@ -1,6 +1,6 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
 
-const cjsPkg = require('../cjs-package/index.js');
+const cjsPkg = require('cjs');
 
 const cjsClass = new cjsPkg.CjsUtilClass(5);
 
@@ -10,9 +10,7 @@ console.log(cjsClass.foo);
 console.log(cjsPkg.cjsUtilFunction('aAbBcC'));
 
 (async function esmImport() {
-  const { EsmUtilClass, esmUtilFunction } = await import(
-    '../esm-package/index.mjs'
-  );
+  const { EsmUtilClass, esmUtilFunction } = await import('esm');
 
   const esmClass = new EsmUtilClass(3);
   console.log(esmClass.foo);
--- Quest07/skeleton/esm-my-project/index.mjs
+++ Quest07/skeleton/esm-my-project/index.mjs
@@ -1,7 +1,7 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
 
-import { EsmUtilClass, esmUtilFunction } from '../esm-package/index.mjs';
-import cjs from '../cjs-package/index.js';
+import { EsmUtilClass, esmUtilFunction } from 'esm';
+import cjs from 'cjs';
 const { CjsUtilClass, cjsUtilFunction } = cjs;
 
 const esmClass = new EsmUtilClass(7);
