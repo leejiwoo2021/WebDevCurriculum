e8dae67b67a20fe4b1f484ed4c935965132b3288
leejiwoo2021 <jiwoo.lee@knowre.com>
2021-07-01 22:54:26 +0900

    feat : Quest15 Client 테스트 코드 작성

--- Quest15/client/test/load.test.ts
+++ Quest15/client/test/load.test.ts
@@ -7,6 +7,8 @@ const instance = axios.create({
   }),
 });
 
+jest.setTimeout(10000);
+
 describe('Index 페이지 접속 확인', () => {
   beforeAll(async () => {
     const loginResponse = await instance.post(
@@ -28,11 +30,17 @@ describe('Index 페이지 접속 확인', () => {
     await page.evaluate((authToken) => {
       localStorage.setItem('token', authToken);
     }, token);
-    await page.goto('https://localhost:3000');
-    // await page.pdf({ path: 'hn.pdf' });
+    await page.goto('https://localhost:3000', { waitUntil: 'domcontentloaded' });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   });
 
-  it('제목 일치 검사 "Editor"', async () => {
+  test('페이지 헤더 제목 검사', async () => {
     await expect(page.title()).resolves.toMatch('Editor');
+  });
+
+  test('페이지 h1태그 제목 검사', async () => {
+    const h1Handle = await page.$('.l-header-title');
+    const html = await page.evaluate((h1Handle) => h1Handle.innerHTML, h1Handle);
+    expect(html).toBe('Text Editor');
   });
 });
